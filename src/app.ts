90ada258c6bdabb7f5ddc0a8d8327e4f59da789f
bruce-x-wu <wu.bruce.x@gmail.com>
2022-10-25 21:10:29 -0400

    feat: add website skeleton

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response } from 'express';
+// import path from 'path'
+// import { fileURLToPath } from 'url';
+
+const app: Application = express();
+// const __filename: string = fileURLToPath(import.meta.url);
+// const __dirname: string = path.dirname(__filename);
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => console.log(`Express is listening at http://localhost:${port}`));
