83783bee7eb7921022aae9a3fa755b34cfdca5a7
bruce-x-wu <wu.bruce.x@gmail.com>
2022-11-02 23:44:15 -0400

    style: configure eslint options for web extensions

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express, { Application, Request, Response } from 'express';
-// import path from 'path'
+// import path from 'path';
 // import { fileURLToPath } from 'url';
 
 const app: Application = express();
